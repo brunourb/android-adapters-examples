8add374571984f8e9d7af07eeecd9d39e36f897a
Bruno Rodrigues <bruno@urbano.eti.br>
2018-04-07 17:53:13 -0300

    Exemplo base adapter e array adapter.

--- app/src/main/java/br/com/senaigo/mobile/vouaprenderandroid/adapters/PessoaAdapter.java
+++ app/src/main/java/br/com/senaigo/mobile/vouaprenderandroid/adapters/PessoaAdapter.java
@@ -1,31 +1,83 @@
 package br.com.senaigo.mobile.vouaprenderandroid.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.com.senaigo.mobile.vouaprenderandroid.R;
+import br.com.senaigo.mobile.vouaprenderandroid.entidades.Pessoa;
 
 /**
  * Created by bruno on 07/04/18.
  */
 
 public class PessoaAdapter extends BaseAdapter {
+
+
+    Context context;
+    List<Pessoa> colecao;
+    LayoutInflater inflter;
+
+    public PessoaAdapter(final Context applicationContext,
+                         final List<Pessoa> colecao) {
+        this.context = applicationContext;
+        this.colecao = colecao;
+
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return this.colecao != null ? this.colecao.size() : 0;
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return this.colecao.get(i);
+    }
+
+    private Pessoa parsetItem(int i){
+        return this.colecao.get(i);
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+
+        // inflate the layout for each list row
+        //'Infla' o layout(pega a referencia) para ser trabalhada
+        //no método
+        if (view == null) {
+            view = LayoutInflater.from(context).
+                    inflate(R.layout.item_pessoa,
+                            viewGroup, false);
+        }
+
+        // pega o objeto corrente da lista
+        Pessoa pessoa = parsetItem(i);
+
+        //Neste ponto vc ira popular os dados do seu layout,
+        //utilizando JAVA.
+
+        TextView campoNome, campoEmail;
+
+        //CASO não queria declarar um campo
+        //((TextView)view.findViewById(R.id.txtItemNome)).setText(pessoa.getNome());
+
+        campoNome = view.findViewById(R.id.txtItemNome);
+        campoEmail = view.findViewById(R.id.txtItemEmail);
+
+        campoNome.setText(pessoa.getNome());
+        campoEmail.setText(pessoa.getEmail());
+
+        return view;
     }
 }
--- app/src/main/java/br/com/senaigo/mobile/vouaprenderandroid/atividades/BaseAdapterExemplo.java
+++ app/src/main/java/br/com/senaigo/mobile/vouaprenderandroid/atividades/BaseAdapterExemplo.java
@@ -2,14 +2,53 @@ package br.com.senaigo.mobile.vouaprenderandroid.atividades;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import br.com.senaigo.mobile.vouaprenderandroid.R;
+import br.com.senaigo.mobile.vouaprenderandroid.adapters.PessoaAdapter;
+import br.com.senaigo.mobile.vouaprenderandroid.entidades.Pessoa;
 
-public class BaseAdapterExemplo extends AppCompatActivity {
+public class BaseAdapterExemplo extends DefaultAtctivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base_adapter_exemplo);
+    }
+
+    public void adicionar(View view) {
+
+        //ENTRADA DE DADOS
+        //Captura o componente EditText da VIEW
+        txtNome = findViewById(R.id.txtNome);
+        txtEmail = findViewById(R.id.txtEmail);
+        txtTelefone = findViewById(R.id.txtTelefone);
+        txtSenha = findViewById(R.id.txtTelefone);
+        txtConfirmaSenha = findViewById(R.id.txtConfirmaSenha);
+
+        String nome, email, telefone, senha, confirmarSenha;
+
+        nome = txtNome.getText().toString();
+        email = txtEmail.getText().toString();
+        telefone = txtTelefone.getText().toString();
+        senha = txtSenha.getText().toString();
+        confirmarSenha = txtConfirmaSenha.getText().toString();
+
+        Pessoa pessoa = new Pessoa(nome, email, telefone, senha);
+
+        lista.add(pessoa);
+
+        //SAIDA
+
+        minhaLista = findViewById(R.id.minhaLista);
+
+        //SAIDA
+
+        minhaLista = findViewById(R.id.minhaLista);
+
+
+        PessoaAdapter pessoaAdapter = new PessoaAdapter(this, lista);
+        minhaLista.setAdapter(pessoaAdapter);
     }
 }
